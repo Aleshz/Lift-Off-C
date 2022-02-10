bcce40a7c9b52727edb759033e5298c3877e0874
Debabrata47 <78600746+Debabrata47@users.noreply.github.com>
2022-02-10 19:52:19 +0530

    week 1

---
+++ Week 1/python_practice.py
@@ -0,0 +1,12 @@
+# import os
+# os.chdir('C:/Users/Lenovo/Desktop/Parking_Space_Counter_with_GUI-main')
+
+# C:\Users\Lenovo\Desktop\Parking_Space_Counter_with_GUI-main
+
+# for f in os.listdir():
+#     file_name,file_ext=os.path.splitext(f)
+#     print(file_name)
+    # print(f)
+
+
+    
